bb3f3ea9ab85a05ebdfd70df1fd2dc93dcea65ba
Marcin Grychtoł <mgmarcingrychtol@gmail.com>
2020-01-30 15:42:13 +0100

    removing .idea from cache

--- src/main/java/com/example/storage/impl/PostgresGameStorage.java
+++ src/main/java/com/example/storage/impl/PostgresGameStorage.java
@@ -4,11 +4,42 @@ import com.example.dto.Game;
 import com.example.dto.Opinion;
 import com.example.storage.GameStorage;
 
+import java.sql.*;
+
 public class PostgresGameStorage extends GameStorage {
 
 
+    private static final String DB_ADDRESS = "jdbc:postgresql://localhost:5432/video_store";
+    private static final String USER_PASS = "password";
+    private static final String USER_NAME = "postgres";
+
     @Override
     public void addGame(Game game) {
+
+        final String ADD_QUERY =
+                "INSERT INTO games (id, producer, name, platform, premiere_date) VALUES" +
+                        "(?,?,?,?,?);";
+
+        Connection connection = initializeConnection();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(ADD_QUERY);
+
+            preparedStatement.setDouble(1,game.getGameId());
+            preparedStatement.setString(2,game.getProducer());
+            preparedStatement.setString(3,game.getName());
+            preparedStatement.setString(4,game.getPlatform().getName());
+            preparedStatement.setDate(5, Date.valueOf(game.getPremiereDate()));
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println("Failed to execute set or execute query.");
+            e.printStackTrace();
+            throw new RuntimeException("Failed to execute set or execute query: " + e.getMessage());
+        } finally {
+            closeConnection(connection, preparedStatement);
+        }
 
     }
 
@@ -25,5 +56,31 @@ public class PostgresGameStorage extends GameStorage {
     @Override
     public Game getGameData(long id) {
         return null;
+    }
+
+    private Connection initializeConnection() {
+        try {
+            return DriverManager.getConnection(DB_ADDRESS, USER_NAME, USER_PASS);
+        } catch (SQLException e) {
+            System.err.println("Failed to initialize connection: " + e.getMessage());
+            throw new RuntimeException("Failed to initialize connection: " + e.getMessage());
+        }
+    }
+
+    private boolean closeConnection(Connection connection, Statement statement) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            e.getMessage();
+            throw new RuntimeException("Error during closing database connection!");
+        }
+
     }
 }
